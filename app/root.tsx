e5e82e624776a7f06c4347133c7270d014ba952f
ychsiao\ychsiao <frank820323@gmail.com>
2025-09-13 00:15:02 +0800

    add antd

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from './+types/root';
 import './app.css';
+import '@ant-design/v5-patch-for-react-19';
 
 export const links: Route.LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },

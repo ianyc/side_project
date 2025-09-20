31a8f673cdbdbf2c9e7771d301470af705628624
WINDOWS-QVSU4GI\user <kitty30603@gmai.com>
2025-09-20 23:19:38 +0800

    onChange固定用法

--- app/modules/dashboard/components/peach.jsx
+++ app/modules/dashboard/components/peach.jsx
@@ -2,34 +2,56 @@ import PropTypes from 'prop-types';
 import { Button } from 'antd';
 import { GiftOutlined } from '@ant-design/icons';
 import { useState } from 'react';
+import { Flex, Input } from 'antd';
 
 const Peach = () => {
-  const [ ifClick, setIfClick ] = useState(false);
+
+  const [inputString, setInputString] = useState('');
+  const [copyString, setCopyString] = useState('');
+
+  const [ifClick, setIfClick] = useState(false);
   const clickMe = () => {
     setIfClick(!ifClick);
+    setCopyString(inputString);
   }
 
 
   return (
     <>
-      <Button color={'red'} variant="outlined"></Button>
-      <Button
-        className="button-field"
-        type="text"
-        icon={<GiftOutlined />}
-        style={{
-          fontSize: '50px',
-          marginTop: '20px',
-          marginLeft: '20px',
-          color: ifClick ? 'red' : 'green',
-        }}
-        onClick={() => {
-          clickMe();
-        }}
+      <Flex style={{ marginTop: '20px' }}>
+        <Input placeholder="請輸入文字"
+          onChange={(e) => {
+            setInputString(e.target.value);
+            console.log(e.target.value);
+          }}
+          style={{
+            width: '20%',
+            marginLeft: '20px',
+          }} />
+        <Button
+          className="button-field"
+          type="text"
+          icon={<GiftOutlined />}
+          style={{
+            fontSize: '30px',
+            marginLeft: '20px',
+            color: ifClick ? 'red' : 'green',
+          }}
+          onClick={() => {
+            clickMe();
+          }}
         // onClick={clickMe()}
-      >
-        123
-      </Button>
+        >
+          複製字串到隔壁
+        </Button>
+        <Input placeholder="等等看我變魔術"
+          variant="filled"
+          value={copyString}
+          style={{
+            width: '20%',
+            marginLeft: '20px',
+          }} />
+      </Flex>
 
     </>
   )
